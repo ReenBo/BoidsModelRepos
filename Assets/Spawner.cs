70e81a69d5b919554951add35f52a092527d6dff
ReenBo <88144545+ReenBo@users.noreply.github.com>
2021-08-01 07:35:50 +0300

    Create

---
+++ Assets/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    static public Spawner S;
+    static public List<Boid> boids;
+
+    [Header("Set in inspector: Spawning")]
+    public GameObject boidPrefab;
+    public Transform boidAnchor;
+    public int numBoids = 100;
+    public float spawnRadius = 100f;
+    public float spawnDelay = 0.1f;
+
+    [Header("Set in Inspector: Boids")]
+    public float velocity = 30f;
+    public float neighborDist = 30f;
+    public float collDist = 10f;
+    public float velMatching = 0.25f;
+    public float flockCentering = 0.2f;
+    public float collAvoid = 4f;
+    public float attractPull = 1f;
+    public float attractPuch = 2f;
+    public float attractPuchDist = 20f;
+
+    private void Awake()
+    {
+        S = this;
+        boids = new List<Boid>();
+        InstantiateBoid();
+    }
+
+    public void InstantiateBoid()
+    {
+        GameObject go = Instantiate(boidPrefab);
+        Boid b = go.GetComponent<Boid>();
+        b.transform.SetParent(boidAnchor);
+        boids.Add(b);
+        if(boids.Count < numBoids)
+        {
+            Invoke("InstantiateBoid", spawnDelay);
+        }
+    }
+}
